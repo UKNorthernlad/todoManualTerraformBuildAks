4c67119b1dcc6398c94a9fc481a6cd971ea09d68
Billy Bunter <billy@bunter.com>
2024-02-22 11:51:41 +0000

    Added RBAC authentication to KeyVault.

--- infra/kv.tf
+++ infra/kv.tf
@@ -7,6 +7,7 @@ resource "azurerm_key_vault" "kv" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled    = false
+  enable_rbac_authorization   = true
 
   depends_on = [azurerm_resource_group.demo-rg]
 
